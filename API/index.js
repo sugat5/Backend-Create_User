5af8cb4784504f760a01fc363380ce506cc29b2c
Sugat <sugatbodade@gmail.com>
2022-12-21 23:10:22 +0530

    BW8|Created Login API

--- API/index.js
+++ API/index.js
@@ -1,51 +1,60 @@
-require("dotenv").config()
-const express=require("express")
-const app=express()
-const bodyParser=require("body-parser")
-const mongodbURL=process.env.DATABASE_URL
-const mongoose=require("mongoose")
-const User = require("./models/users")
-mongoose.set("strictQuery",true)
-const jsonParser=bodyParser.json()
-const { v4: uuidv4 } = require('uuid');
-const dataBase=mongoose.connect(mongodbURL)
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongodbURL = process.env.DATABASE_URL;
+const mongoose = require("mongoose");
+const User = require("./models/users");
+mongoose.set("strictQuery", true);
+const jsonParser = bodyParser.json();
+const { v4: uuidv4 } = require("uuid");
+const dataBase = mongoose.connect(mongodbURL);
 
-app.post("/signup",jsonParser,async(req,res)=>{
-    const existingUser= await User.findOne({email:req.body.email})
-    if(existingUser){
-          return res.send({error:"User Already Exist,Please Login"}).statusCode(400)
-    }
-    const authToken=uuidv4();
-    try {const newUser= new User({...req.body,authToken:[authToken]})
-    newUser.save()}
-    catch{(error)=>{
-        return res.send(error).status(400)
-    }}
-    res.send({msg:"User Created Successfully",authToken})
-})
+app.post("/signup", jsonParser, async (req, res) => {
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (existingUser) {
+    return res
+      .send({ error: "User Already Exist,Please Login" })
+      .status(400);
+  }
+  const authToken = uuidv4();
+  try {
+    const newUser = new User({ ...req.body, authToken: [authToken] });
+    newUser.save();
+  } catch {
+    (error) => {
+      return res.send(error).status(400);
+    };
+  }
+  res.send({ msg: "User Created Successfully", authToken });
+});
 
-app.post("/signin",jsonParser,async(req,res)=>{
-    const user= await User.findOne({email:req.body.email,password:req.body.password})
-    if(user){
-        const authToken=uuidv4()
-        res.send({...req.body,authToken:[authToken]})
-        
-}else{
-    res.send({msg:"User Not Found,Please Signup"})
-}
+app.post("/signin", jsonParser, async (req, res) => {
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (!existingUser) {
+    return res.send({ msg: "User Not Found,Please Signup" }).status(400)
+  }
+  if (existingUser.password !== req.body.password) {
+    return res.send({ error: "Invalid Password" }).status(400)
+  }
+  const authToken = uuidv4()
+  existingUser.authToken.push(authToken);
+  await existingUser.save();
+  return res.send({ msg: "Login success", authToken });
+    
+  }
+);
 
+app.get("/", (req, res) => {
+  res.send("Hii My Name is Suga");
+});
 
-})
-
-app.get("/",(req,res)=>{
-res.send("Hii My Name is Suga")
-})
-
-
-dataBase.then(()=>{
-    app.listen(3000,(req,res)=>{
-        console.log("Server Started")
-        })
-}).catch((err)=>{
-    console.log("DB connection failed", err)
-})
+dataBase
+  .then(() => {
+    app.listen(3000, (req, res) => {
+      console.log("Server Started");
+    });
+  })
+  .catch((err) => {
+    console.log("DB connection failed", err);
+  });
